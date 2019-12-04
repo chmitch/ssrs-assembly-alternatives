9b501760c3d9f700589b4ddb78f1d8ece7a54a13
Chris Mitchell (OCP) <chmitch@microsoft.com>
2019-12-04 15:33:41 -0600

    Updating the Translate sample to use coded parameters, and account for the fact that when using a cognitive services key and calling the translator endpoing you must provide the region of your service.   This is required because the trasnlator api endpoint does not have a different uri per region even thought the cognitive endpoint is deployed regionally.

--- Scripts/CreateAPIConfigTable.sql
+++ Scripts/CreateAPIConfigTable.sql
@@ -2,10 +2,11 @@ CREATE TABLE APIConfig
 	(
 	apiId varchar(50),
 	apiUri varchar(50),
-	apiKey varchar(1000)
+	apiKey varchar(1000),
+	apiRegion varchar(50) NULL
 	);
 
 INSERT APIConfig VALUES
-('AzureSentiment','https://westus2.api.cognitive.microsoft.com/','<keyvalue>'),
-('AzureTranslate','https://api.cognitive.microsofttranslator.com',',<keyvalue>'),
-('HelloWorld','<fn url>','<keyvalue>')
+('AzureSentiment','https://westus2.api.cognitive.microsoft.com/','<keyvalue>',NULL),
+('AzureTranslate','https://api.cognitive.microsofttranslator.com',',<keyvalue>','westus2'),
+('HelloWorld','<fn url>','<keyvalue>',NULL)
