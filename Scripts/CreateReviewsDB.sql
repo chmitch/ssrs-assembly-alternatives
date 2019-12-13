eb9bbb6168ac3372970ce44bbbc54e2df1aa3db9
alexkarasek <48524367+alexkarasek@users.noreply.github.com>
2019-12-13 15:27:43 -0500

    Update CreateReviewsDB.sql

--- Scripts/CreateReviewsDB.sql
+++ Scripts/CreateReviewsDB.sql
@@ -1,25 +1,43 @@
 CREATE TABLE Reviews
 	(
 	ReviewId int,
-	ReviewText varchar(1000)
+	ReviewText varchar(1000),
+	JSONData nvarchar(max)
 	);
 
-INSERT Reviews VALUES
-(1,'  ==You''re cool==  You seem like a really cool guy... *bursts out laughing at sarcasm*.'),
-(2,'I just want to point something out (and I''m in no way a supporter of the strange old git), but he is referred to as Dear Leader, and his father was referred to as Great Leader.'),
-(3,'  ==RUDE== Dude, you are rude upload that carl picture back, or else.'),
-(4,'  : I know you listed your English as on the "level 2", but don''t worry, you seem to be doing nicely otherwise, judging by the same page - so don''t be taken aback. I just wanted to know if you were aware of what you wrote, and think it''s an interesting case. : I would write that sentence simply as "Theoretically I am an altruist, but only by word, not by my actions.". : PS. You can reply to me on this same page, as I have it on my watchlist.  '),
-(5,'  == Conflict of interest ==  You are a person who is doing some sort of harm to this lady Saman Hasnain.. It is apparent that you are making sure that her name is defamed.... Okay no problem... Will get a better source... you are playing dirty... DOG Sonisona '),
-(6,' :the category was unnecesary, as explained in my edit summary. Your threats are disgrace to wikipedia. '),
-(7,' :You call MacDonald''s a part of your "culture"? Nonsense! Spend some 10 years in France, and then you will have a hint of what Culture is!   '),
-(8,'Grow up you biased child.  '),
-(9,'   Stop trolling, zapatancas, calling me a liar merely demonstartes that you arer Zapatancas. You may choose to chase every legitimate editor from this site and ignore me but I am an editor with a record that isnt 99% trolling and therefore my wishes are not to be completely ignored by a sockpuppet like yourself. The consensus is overwhelmingly against you and your trollin g lover Zapatancas,  '),
-(10,'I saw it before watching the episode. Oh well.'),
-(11,'This is British form and does not correspond to French nobiliary rules, which, in any case, are defunct, given that French noble titles were rendered obsolete more than a century ago. I think that, technically, she is merely Raine Spencer, having retrieved her previous surname upon her divorce from Chambrun. (And during the French marriage, she was not Countess of Chambrun, she was Countess Jean-Francois de Chambrun, and, as per French usage, would be referred to as Mme de Chambrun, with the title used only by servants and so-called inferiors.)  '),
-(12,'Today was the worst day ever'),
-(13,'I''m so happy'),
-(14,'This is the best game I''ve ever played'),
-(15,'This game is so dang good man'),
-(16,'Such an incredible game. Absolutely loved it.'),
-(17,'Until the next game comes out, this game is undisputedly the best Xbox game of all time'),
-(18,'This game without that feature would be the worst fighting game ever. Y''all really overrate that franchise.');
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (1, N'  ==You''re cool==  You seem like a really cool guy... *bursts out laughing at sarcasm*.', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (2, N'I just want to point something out (and I''m in no way a supporter of the strange old git), but he is referred to as Dear Leader, and his father was referred to as Great Leader.', N' {''source'': ''data entry'',''author'': ''somebody@demo.com'',''CreatedDate'': ''2013-01-20T00:00:00Z''}')
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (3, N'  ==RUDE== Dude, you are rude upload that carl picture back, or else.', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (4, N'  : I know you listed your English as on the "level 2", but don''t worry, you seem to be doing nicely otherwise, judging by the same page - so don''t be taken aback. I just wanted to know if you were aware of what you wrote, and think it''s an interesting case. : I would write that sentence simply as "Theoretically I am an altruist, but only by word, not by my actions.". : PS. You can reply to me on this same page, as I have it on my watchlist.  ', N' {''author'': ''somebody@demo.com''}')
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (5, N'  == Conflict of interest ==  You are a person who is doing some sort of harm to this lady Saman Hasnain.. It is apparent that you are making sure that her name is defamed.... Okay no problem... Will get a better source... you are playing dirty... DOG Sonisona ', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (6, N' :the category was unnecesary, as explained in my edit summary. Your threats are disgrace to wikipedia. ', N' {''author'': ''somebody@demo.com''}')
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (7, N' :You call MacDonald''s a part of your "culture"? Nonsense! Spend some 10 years in France, and then you will have a hint of what Culture is!   ', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (8, N'Grow up you biased child.  ', N' {''source'': ''data entry'',''author'': ''somebody@demo.com'',''CreatedDate'': ''2013-01-20T00:00:00Z''}')
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (9, N'   Stop trolling, zapatancas, calling me a liar merely demonstartes that you arer Zapatancas. You may choose to chase every legitimate editor from this site and ignore me but I am an editor with a record that isnt 99% trolling and therefore my wishes are not to be completely ignored by a sockpuppet like yourself. The consensus is overwhelmingly against you and your trollin g lover Zapatancas,  ', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (10, N'I saw it before watching the episode. Oh well.', N' {''source'': ''data entry'',''author'': ''somebody@demo.com'',''CreatedDate'': ''2013-01-20T00:00:00Z''}')
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (11, N'This is British form and does not correspond to French nobiliary rules, which, in any case, are defunct, given that French noble titles were rendered obsolete more than a century ago. I think that, technically, she is merely Raine Spencer, having retrieved her previous surname upon her divorce from Chambrun. (And during the French marriage, she was not Countess of Chambrun, she was Countess Jean-Francois de Chambrun, and, as per French usage, would be referred to as Mme de Chambrun, with the title used only by servants and so-called inferiors.)  ', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (12, N'Today was the worst day ever', N' {''source'': ''data entry'',''author'': ''somebody@demo.com'',''CreatedDate'': ''2013-01-20T00:00:00Z''}')
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (13, N'I''m so happy', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (14, N'This is the best game I''ve ever played', N' {''CreatedDate'': ''2013-01-20T00:00:00Z'', ''source'': ''data entry''}')
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (15, N'This game is so dang good man', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (16, N'Such an incredible game. Absolutely loved it.', N' {''source'': ''sourcery'',''author'': ''bob@demo.com'',''CreatedDate'': ''2013-01-20T00:00:00Z''}')
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (17, N'Until the next game comes out, this game is undisputedly the best Xbox game of all time', NULL)
+GO
+INSERT [dbo].[Reviews] ([ReviewId], [ReviewText], [JSONData]) VALUES (18, N'This game without that feature would be the worst fighting game ever. Y''all really overrate that franchise.', N' {''author'': ''somebody@demo.com'',''CreatedDate'': ''2013-01-20T00:00:00Z'', ''Tags'' : [''Red'', ''Big'', ''Fun'']}')
+GO
