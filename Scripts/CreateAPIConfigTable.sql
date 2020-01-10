f7c2b4793ff2889c1ae2187ecc55a11c16180f48
alexkarasek <48524367+alexkarasek@users.noreply.github.com>
2020-01-10 17:21:21 -0500

    Update CreateAPIConfigTable.sql

--- Scripts/CreateAPIConfigTable.sql
+++ Scripts/CreateAPIConfigTable.sql
@@ -9,5 +9,5 @@ CREATE TABLE APIConfig
 INSERT APIConfig VALUES
 ('AzureSentiment','https://eastus2.api.cognitive.microsoft.com/','ba52cee2c8254a4a95ae2d581fe7ab12',NULL),
 ('AzureTranslate','https://api.cognitive.microsofttranslator.com','ba52cee2c8254a4a95ae2d581fe7ab12','westus2'),
-('HelloWorld','https://ssrsfunction20191219092530.azurewebsites.net/api/HelloWorld','aE9UkBuRhXP8ygafFd3lKR0c7BZo7Bq/9SpVXesrRW5ZcJCNGvR1Tw==',NULL),
-('JSONConverter','https://ssrsfunction20191219092530.azurewebsites.net/api/JSONConverter','aE9UkBuRhXP8ygafFd3lKR0c7BZo7Bq/9SpVXesrRW5ZcJCNGvR1Tw==',NULL)
+('HelloWorld','https://ssrsfunction20191219092530.azurewebsites.net','aE9UkBuRhXP8ygafFd3lKR0c7BZo7Bq/9SpVXesrRW5ZcJCNGvR1Tw==',NULL),
+('JSONConverter','https://ssrsfunction20191219092530.azurewebsites.net','aE9UkBuRhXP8ygafFd3lKR0c7BZo7Bq/9SpVXesrRW5ZcJCNGvR1Tw==',NULL)
